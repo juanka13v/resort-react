4b60486e4bbdb074ee6762210fc1de9206cbe967
Juan Valero <valerojuan13c@gmail.com>
2021-12-16 16:01:45 -0300

    Dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { SliderData } from "./data/SliderData";
@@ -5,10 +7,17 @@ import GlobalStyle from "./globalStyles";
 
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <GlobalStyle/>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData}/>
     </>
   )
