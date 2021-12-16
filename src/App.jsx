142238e98171aefc68b9557bf1670e345da362f8
Juan Valero <valerojuan13c@gmail.com>
2021-12-16 10:11:55 -0300

    Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import { SliderData } from "./data/SliderData";
+import GlobalStyle from "./globalStyles";
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Navbar />
+      <Hero slides={SliderData}/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import styled, {css} from "styled-components/macro";
+import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import Bars from '../images/menu-bars.svg'
+
+const Nav = styled.nav`
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
+
+const MenuBars = styled.i`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contains;
+    height: 40px;
+    width: 40px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+    cursor: pointer;
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex; 
+  align-items: center;
+  margin-right: -40px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Logo to={"/"}>ELIXR</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to='/contact' primary='true'>
+          Contact Us
+        </Button>
+      </NavBtn>
+    </Nav>
+  );
+};
+
+export default Navbar;
